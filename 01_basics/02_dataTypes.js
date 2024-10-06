3a7c2662bd027d553987a5f606d7065e52982c24
Lav Sarkari <luv.sarkari@gmail.com>
2024-10-06 12:11:31 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // not allowed at node.js
+
+console.log(9+8)
+
+
+let name = "kairav"
+let age = 18
+let isLoggedIn= false
+
+//number => 2 to power 53 (range)
+//bigint
+//string => ''""
+//boolean => true/fasle
+//null => stand alone value
+//undefiend => 
+//symbol => unique 
+
+
+//object
+
+console.log(typeof(name));
