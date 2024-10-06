4c9d59e6bd4bc4ed2e931dd1c15ab7e1a9009435
Lav Sarkari <luv.sarkari@gmail.com>
2024-10-06 11:57:55 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 1234
+let accountEmail = "LavSarkari@pm.me"
+var accountPassword = "12345"
+accountCity = "Lucknow"
+let accountState;
+console.table([accountEmail, accountID, accountPassword, accountCity, accountState])
+
+// accountID = 2 //NOT ALLOWED
+accountEmail = "Luv.Sarkari@gmail.com"
+accountPassword = "newPassword"
+accountCity = "Gorakhpur"
+// console.log(accountID);
+console.table([accountEmail, accountID, accountPassword, accountCity, accountState])
